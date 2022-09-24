c83ab412cdbf357285695926d6f423b15a16740d
FvsionAI <113994073+fvsionAI@users.noreply.github.com>
2022-09-24 13:01:21 +0800

    update to mention node version

--- py/testing.py
+++ py/testing.py
@@ -5,6 +5,11 @@ from app.endpoints.modules import utils
 # Utility imports
 import PIL.Image, PIL.ImageOps
 
+import torch
+from transformers import pipeline
 
-img = PIL.Image.open('output/example/color_spectrum.png').convert("RGB")
-utils.RGBColorReplacement(img, (255,255,255), (0, 0, 0))
+speech_recognizer = pipeline("automatic-speech-recognition", model="facebook/wav2vec2-base-960h")
+
+
+# img = PIL.Image.open('output/example/color_spectrum.png').convert("RGB")
+# utils.RGBColorReplacement(img, (255,255,255), (0, 0, 0))
