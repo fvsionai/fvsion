cca10c693068791a0a5ac7be4f8140eb14702ac1
FvsionAI <113994073+fvsionAI@users.noreply.github.com>
2022-09-25 03:11:15 +0800

    add getAPI

---
+++ src/utils/index.ts
@@ -0,0 +1,13 @@
+export * from "./app.config";
+
+export const API_library = {
+  root: "http://localhost:4242/api/",
+  version: "v00",
+  functionList: ["txt2img", "img2img", "inpainting", "pid"],
+};
+
+export function getAPI(s: string) {
+  if (API_library.functionList.includes(s)) {
+    return API_library.root + "/" + API_library.version + "/" + s;
+  }
+}
