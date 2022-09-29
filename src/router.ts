0617a240ab1f0e7e6235182790f5f1bdd475a3e9
Hafiidz <3688500+Hafiidz@users.noreply.github.com>
2022-09-29 08:52:17 +0800

    working but need to rework to help with merge

--- src/router.ts
+++ src/router.ts
@@ -2,21 +2,19 @@ import {
   createRouter,
   createWebHistory,
   createWebHashHistory,
+  createMemoryHistory
 } from "vue-router";
+import HomeView from "@/views/HomeView.vue"
 
 console.log(import.meta.env);
 
 const router = createRouter({
-  history: import.meta.env.PROD ? createWebHashHistory() : createWebHistory(),
+  history: import.meta.env.PROD ? createWebHashHistory('/') : createWebHistory(),
   routes: [
     {
       path: "/",
-      component: () => import("@/views/HomeView.vue"),
-    },
-    {
-      path: "/img2img/",
-      component: () => import("@/views/Img2ImgView.vue"),
-    },
+      component: HomeView,
+    }
   ],
 });
 
